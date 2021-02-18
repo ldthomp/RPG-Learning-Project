4d225820d8b0fe4e4dfc651557f32745ca0e49d7
DESKTOP-N0C78OI\Adam <l.d.thompson215@gmail.com>
2021-02-18 17:33:15 +0000

    finished sandbox 2 prototype and added scene management portal

---
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int sceneToLoad = 1;
+        private void Start()
+        {
+            print(SceneManager.GetActiveScene());
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            //old code used to load scenes (project sub)
+            //int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            //int nextSceneIndex = currentSceneIndex + 1;
+            //if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+            //{
+            //    nextSceneIndex = 0;
+            //}
+            if (other.tag == "Player")
+            {
+
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+    }
+}
