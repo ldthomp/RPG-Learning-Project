47454fa4fa58efccbd4e14b88f4d4d0f95387b1e
DESKTOP-N0C78OI\Adam <l.d.thompson215@gmail.com>
2021-03-09 12:04:47 +0000

    Fixed folder and script issues. Lesson on saving and loading using FileStream, FileMode, and Encoding.UTF8

---
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+namespace RPG.Saving
+{
+    public class SavingSystem : MonoBehaviour
+    {
+        public void Save(string saveFile)
+        {
+            string path = GetPathFromSaveFile(saveFile);
+            print("saving to " + path);
+            using (FileStream stream = File.Open(path, FileMode.Create))
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes("¡Hola Mundo!");
+                stream.Write(bytes, 0, bytes.Length);
+            }
+                        
+        }
+        public void Load(string saveFile)
+        {
+
+            string path = GetPathFromSaveFile(saveFile);
+            print("Loading to " + path);
+
+            using(FileStream stream = File.Open(path, FileMode.Open))
+            {
+                byte[] buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+                string message = Encoding.UTF8.GetString(buffer);
+                print(message);
+            }
+
+        }
+        private string GetPathFromSaveFile(string saveFile)
+        {
+            return Path.Combine(Application.persistentDataPath, saveFile + ".sav");
+        }
+    }
+}
--- Assets/Scripts/Scene Management/Portal.cs
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -1,31 +1,72 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
-        [SerializeField] int sceneToLoad = 1;
-        private void Start()
+        enum DestinationIdentifier
         {
-            print(SceneManager.GetActiveScene());
+            A,B,C,D,E,F,G
         }
+        [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint; //future possibility to change to getcomponentinchildren & find gamobject Spawnpoint
+        [SerializeField] DestinationIdentifier destination;
+        [SerializeField] float fadeOutTime = 1f;
+        [SerializeField] float fadeInTime = 1f;
+        [SerializeField] float fadeWaitTime = 0.5f;
         private void OnTriggerEnter(Collider other)
         {
-            //old code used to load scenes (project sub)
-            //int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-            //int nextSceneIndex = currentSceneIndex + 1;
-            //if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
-            //{
-            //    nextSceneIndex = 0;
-            //}
             if (other.tag == "Player")
             {
-
-                SceneManager.LoadScene(sceneToLoad);
+                StartCoroutine(Transition());
             }
+        }
+        private IEnumerator Transition()
+        {
+            if(sceneToLoad < 0)
+            {
+                Debug.LogError("scene to load not set.");
+                yield break;
+            }
+
+            DontDestroyOnLoad(gameObject);
+
+            Fader fader = FindObjectOfType<Fader>();
+
+            yield return fader.FadeOut(fadeOutTime);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            yield return new WaitForSeconds(fadeWaitTime);
+            yield return fader.FadeIn(fadeInTime);
+
+            Destroy(gameObject);
+        }
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.destination != destination) continue;
+                
+                return portal;
+                
+            }
+            return null;
+        }
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            //player.transform.position = otherPortal.spawnPoint.position;
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
         }
     }
 }
