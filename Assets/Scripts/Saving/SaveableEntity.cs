3d27045c8d778c2c3b6fb1ee9d0a0d9e16881bf0
DESKTOP-N0C78OI\Adam <l.d.thompson215@gmail.com>
2021-03-16 14:04:40 +0000

    Creating universally unique identifier with Saveable Entity script

---
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Saving
+{
+    [ExecuteAlways]
+    public class SaveableEntity : MonoBehaviour
+    {
+        [SerializeField] string uniqueIdentifier = "";
+        public string GetUniqueIdentifier()
+        {
+            return " ";
+        }
+        public object CaptureState()
+        {
+            print("Capturing state" + GetUniqueIdentifier());
+            return null;
+        }
+        public void RestoreStates(object state)
+        {
+            print("restoring state for " + GetUniqueIdentifier());
+        }
+
+        private void Update()
+        {
+            if (Application.IsPlaying(gameObject)) return;
+            
+            print("Editing");
+            
+        }
+    }
+}
