23c2ccb5da06b321157e63407b9570fdbd7b8913
DESKTOP-N0C78OI\Adam <l.d.thompson215@gmail.com>
2021-04-13 18:44:21 +0100

    Lectures Completed: 116 to 125 Includes - projectile, arrow and fireball, damage and VFX

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -89,9 +89,23 @@ namespace RPG.Combat
         void Hit()
         {
             if (target == null) return;
-            target.TakeDamage(currentWeapon.GetDamage());
+
+            if (currentWeapon.HasProjectile())
+            {
+                currentWeapon.LaunchProjectile(rightHandTransform, leftHandTransform, target);
+            }
+            else
+            {
+                target.TakeDamage(currentWeapon.GetDamage());
+            }
+
             print("Enemy Taking a Hit");
         }
+
+        void Shoot()
+        {
+            Hit();
+        }    
 
         private bool GetIsInRange()
         {
---
+++ Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,78 @@
+﻿using RPG.Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Projectile : MonoBehaviour
+    {
+        [SerializeField] float projectileSpeed = 1;
+        [SerializeField] bool isHomingProjectile = true;
+
+        [SerializeField] float maxLifeTime = 3;
+        [SerializeField] float lifeAfterImpact = 2;
+
+        [SerializeField] GameObject hitEffect;
+        [SerializeField] GameObject[] destroyOnHit = null;
+        Health target = null;
+        float damage = 0;
+
+        private void Start()
+        {
+            transform.LookAt(GetAimLocation());
+        }
+
+        private void Update()
+        {
+            if (target == null) return;
+
+
+            if (isHomingProjectile && !target.IsDead())
+            {
+                transform.LookAt(GetAimLocation());
+            }
+            transform.Translate(Vector3.forward * projectileSpeed * Time.deltaTime);
+        }
+        public void SetTarget(Health target, float damage)
+        {
+            this.target = target;
+            this.damage = damage;
+
+            Destroy(gameObject, maxLifeTime);
+        }
+
+        private Vector3 GetAimLocation()
+        {
+            CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
+
+            if (targetCapsule == null)
+            {
+                return target.transform.position;
+            }
+            return target.transform.position + Vector3.up * targetCapsule.height / 2;
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<Health>() != target) return;
+            if (target.IsDead()) return;
+            target.TakeDamage(damage);
+
+            projectileSpeed = 0;
+
+            if(hitEffect != null)
+            {
+                Instantiate(hitEffect, GetAimLocation(), transform.rotation);
+            }
+
+            foreach(GameObject toDestroy in destroyOnHit)
+            {
+                Destroy(toDestroy);
+            }
+
+            Destroy(gameObject, lifeAfterImpact);
+        }
+    }
+
+}
